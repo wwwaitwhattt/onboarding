1a47e51b6a7d191d63c1220f23fccd4e175e98a5
alina t. <kekma228@gmail.com>
2025-05-12 10:36:17 +0300

    context.js: solved 2/2

--- 1-sprint/context.js
+++ 1-sprint/context.js
@@ -57,3 +57,74 @@ let ladder = {
 //  чтобы их вызов можно было сделать по цепочке, например так:
 
 ladder.up().up().down().showStep().down().showStep(); 
+
+// 3 Исправьте выделенную строку, чтобы всё работало
+//   (других строк изменять не надо).
+
+function askPassword(ok, fail) {
+  let password = prompt("Password?", '');
+  if (password == "rockstar") ok();
+  else fail();
+}
+
+let user = {
+  name: 'Вася',
+
+  loginOk() {
+    alert(`${this.name} logged in`);
+  },
+
+  loginFail() {
+    alert(`${this.name} failed to log in`);
+  },
+
+};
+
+// askPassword(user.loginOk, user.loginFail); ИЗМЕНИТЬ
+
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+
+
+// 4 Объект user был изменён. Теперь вместо двух функций
+//   loginOk/loginFail у него есть только одна – user.login(true/false).
+
+//   Что нужно передать в вызов функции askPassword в коде ниже,
+//   чтобы она могла вызывать функцию user.login(true) как ok и функцию
+//   user.login(false) как fail?
+
+function askPassword(ok, fail) {
+  let password = prompt("Password?", '');
+  if (password == "rockstar") ok();
+  else fail();
+}
+
+let user1 = {
+  name: 'John',
+
+  login(result) {
+    alert( this.name + (result ? ' logged in' : ' failed to log in') );
+  }
+};
+
+// askPassword(?, ?); ???
+// Ваши изменения должны затрагивать только выделенный фрагмент кода.
+
+askPassword(user.login.bind(user1, true), user.login.bind(user1, false));
+
+
+// 5 создадим функцию без контекста this, но ссылающуюся на него. 
+//   исправить код
+
+const book = {
+  title: 'Brave New World',
+  author: 'Aldous Huxley',
+}
+
+function summary() {
+  console.log(`${this.title} was written by ${this.author}.`)
+}
+
+// summary()
+
+summary.call(book) // или
+summary.apply(book)
