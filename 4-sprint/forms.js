649cd0a11e588a9a1c38253299ab6201d3bbf9a1
alina t. <kekma228@gmail.com>
2025-06-16 09:17:05 +0300

    4-sprint start

---
+++ 4-sprint/forms.js
@@ -0,0 +1,14 @@
+// 1 
+
+let form = document.forms.my; 
+let elem = form.elements.one; 
+
+// alert(elem.value); // 1
+
+// 2
+// Выведите значение и текст выбранного пункта.
+// Добавьте пункт: <option value="classic">Классика</option>.
+// Сделайте его выбранным.
+
+
+
