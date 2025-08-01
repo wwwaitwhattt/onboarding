7b6ac0a92965c032a9a32139ac039dd00e03c384
Алина Токова <wwwaitwhattt@gmail.com>
2025-08-01 11:46:21 +0300

    intro to vuejs

---
+++ vue_project/task1.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>App</h1>
+  <hr>
+
+  <input type="text" placeholder="Введите текст" v-model="clue" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clue: 'enter text',
+    }
+  }
+}
+</script>
